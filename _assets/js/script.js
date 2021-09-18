fa4b0420127567d1dde2fd5a677584d661f9c8ab
Mehdi Marouani <Mehdi.Marouani@vistaprint.com>
2021-09-18 17:52:15 +0100

    - small change

--- _assets/js/script.js
+++ _assets/js/script.js
@@ -1,11 +1,4 @@
 $(function () {
-  /** open /close menu */
-  $(".navbar-toggler").click(function () {
-    let expended = $(this).attr("aria-expanded");
-    $(this).attr("aria-expanded", "" + expended === "false");
-    $("#navbar-content").toggle(500);
-  });
-
   $(".navbar-nav .nav-link").click(function () {
     $(".navbar-toggler").attr("aria-expanded", "false");
     $("#navbar-content").hide(500);
